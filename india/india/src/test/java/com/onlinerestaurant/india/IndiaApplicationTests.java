961a3e1d12be3031ef69bdf699bdac24c6b2e52e
sohamdatey <sohamdatey22@gmail.com>
2019-03-17 21:28:34 +0530

    first online restaurant commit india

---
+++ india/india/src/test/java/com/onlinerestaurant/india/IndiaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.onlinerestaurant.india;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IndiaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
